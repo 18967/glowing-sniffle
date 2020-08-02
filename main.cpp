2740559c1b8ed936a23af84ef520586aeae66e54
M. Gardner <magardner2010@gmail.com>
2020-08-02 12:40:10 +0300

    move function to library

--- main.cpp
+++ main.cpp
@@ -4,31 +4,6 @@
 
 #include "star.hpp"
 
-template<unsigned S, unsigned X, unsigned Y>
-void connect_closest( starfield<S, X, Y>& s )
-{
-	unsigned n = S;
-	float dists[n][n];
-	for( int i = 0; i < n; i++ ) {
-		for( int j = 0; j < n; j++ ) {
-			dists[i][j] = s.get_star_i( i ) - s.get_star_i( j );
-		}
-	}
-	int i, j, k;
-	float m;
-	for( i = 0; i < n; i++ ) {
-		k = 0;
-		m = dists[i][0];
-		for( j = 1; j < n; j++ ) {
-			if( dists[i][j] < m && i != j && !s.get_constellation( i, j ) ) {
-				m = dists[i][j];
-				k = j;
-			}
-		}
-		s.connect( i, k );
-	}
-}
-
 int main( int argc, char **argv )
 {
 	srand( time( 0 ) );
--- star.hpp
+++ star.hpp
@@ -130,4 +130,31 @@ std::ostream &operator <<( std::ostream& o, starfield<S, X, Y>& s )
 	return o;
 }
 
+
+template<unsigned S, unsigned X, unsigned Y>
+void connect_closest( starfield<S, X, Y>& s )
+{
+	unsigned n = S;
+	float dists[n][n];
+	for( int i = 0; i < n; i++ ) {
+		for( int j = 0; j < n; j++ ) {
+			dists[i][j] = s.get_star_i( i ) - s.get_star_i( j );
+		}
+	}
+	int i, j, k;
+	float m;
+	for( i = 0; i < n; i++ ) {
+		k = 0;
+		m = dists[i][0];
+		for( j = 1; j < n; j++ ) {
+			if( dists[i][j] < m && i != j && !s.get_constellation( i, j ) ) {
+				m = dists[i][j];
+				k = j;
+			}
+		}
+		s.connect( i, k );
+	}
+}
+
+
 #endif
