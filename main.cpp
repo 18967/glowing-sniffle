8657566816e2e78b3f1cfeb0aa7f6612159bff56
M. Gardner <magardner2010@gmail.com>
2020-08-02 12:09:07 +0300

    make constellation function and add sample

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 
 #include "star.hpp"
 
-int main( int argc, char **argv )
+template<unsigned S, unsigned X, unsigned Y>
+void connect_closest( starfield<S, X, Y>& s )
 {
-	srand( time( 0 ) );
-	const int n = 100;
-	starfield<n> s;
+	unsigned n = S;
 	float dists[n][n];
 	for( int i = 0; i < n; i++ ) {
 		for( int j = 0; j < n; j++ ) {
@@ -21,13 +20,24 @@ int main( int argc, char **argv )
 		k = 0;
 		m = dists[i][0];
 		for( j = 1; j < n; j++ ) {
-			if( dists[i][j] < m && i != j ) {
+			if( dists[i][j] < m && i != j && !s.get_constellation( i, j ) ) {
 				m = dists[i][j];
 				k = j;
 			}
 		}
 		s.connect( i, k );
 	}
+}
+
+int main( int argc, char **argv )
+{
+	srand( time( 0 ) );
+	const int n = 150;
+	starfield<n, 1600, 250> s;
+	std::cout << s << std::endl;
+	connect_closest( s );
+	std::cout << s << std::endl;
+	connect_closest( s );
 	std::cout << s << std::endl;
 
 	return 0;
